33d4dea8c7c0ff9e6cbf6a0ed34e177fa6c78156
Matt Olson <matt.j.olson@gmail.com>
2021-08-07 20:55:58 +0000

    looks like it works

--- new-blog/site.hs
+++ new-blog/site.hs
@@ -18,13 +18,15 @@ main = hakyll $ do
     match (fromList ["about.markdown", "contact.markdown"]) $ do
         route   $ setExtension "html"
         compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= loadAndApplyTemplate "templates/default.html" 
+                defaultContext
             >>= relativizeUrls
 
     match "posts/*" $ do
         route $ setExtension "html"
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
+            >>= saveSnapshot "content"
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
 
@@ -33,13 +35,15 @@ main = hakyll $ do
         compile $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
+                    listField "posts" postCtx (return posts) <>
+                    constField "title" "Archives"            <>
                     defaultContext
 
             makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/archive.html" 
+                    archiveCtx
+                >>= loadAndApplyTemplate "templates/default.html" 
+                    archiveCtx
                 >>= relativizeUrls
 
 
@@ -53,14 +57,32 @@ main = hakyll $ do
 
             pandocCompiler
                 >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= loadAndApplyTemplate "templates/default.html"
+                    indexCtx
                 >>= relativizeUrls
 
     match "templates/*" $ compile templateBodyCompiler
 
+    create ["rss.xml"] $ do
+        route idRoute
+        compile $ do
+            let feedCtx = postCtx <> bodyField "description"
+            posts <- fmap (take 10) . recentFirst =<<
+                loadAllSnapshots "posts/*" "content"
+            renderRss feedConfig feedCtx posts
 
---------------------------------------------------------------------------------
+
+------------------------------------------------------------------------
 postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
+
+feedConfig :: FeedConfiguration
+feedConfig = FeedConfiguration
+    { feedTitle = "Audax Labs"
+    , feedDescription =
+        "A short sturdy weblog, fond of food and industry"
+    , feedAuthorName = "Matt Olson"
+    , feedRoot = "https://blog.audax-labs.com"
+    }
